ab4e92ef7b51adbf8dfa0dd986dd765648d6dc5f
mechanist1 <mlaouhiilyess@gmail.com>
2023-03-21 15:24:54 +0100

    first real change in the backend ,after using express generator, i did the data binding from the front end to the backend , so i got the informations that i got from the frontend to the backend, git checkout backendgit checkout backendgit checkout backendgit checkout backendgit checkout backendgit checkout backendgit checkout backendgit checkout backendgit checkout backend! still need to correct the data binding

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.post('/', (req, res) => {
+  const data = req.body; // Extract data from request body
+  console.log(data); // Log data to console
+  res.send('Received data!!!!!'); // Send response back to clients
 });
+
 
 module.exports = router;
