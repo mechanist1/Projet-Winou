8fa8ff30ce1de9e2f81a8ddc44bb0796642c0a6f
mechanist1 <mlaouhiilyess@gmail.com>
2023-03-22 15:16:53 +0100

    finished getting all other information, also fixee the http request issue

---
+++ signin.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.post('/sign-in', (req, res) => {
+  const data = req.body; // Extract data from request body
+  console.log(data); // Log data to console
+  res.send('Received data!!!!!'); // Send response back to clients
+});
+
+
+module.exports = router;
