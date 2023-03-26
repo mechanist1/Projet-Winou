5462fc3cda92ef6107f8787caf21ec4c15d26518
mechanist1 <mlaouhiilyess@gmail.com>
2023-03-26 15:19:19 +0100

    connected to db and saved the members for the signup, did great advancement , still gotta do the signin and post article connection to db

--- signup.js
+++ signup.js
@@ -1,10 +1,49 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require("mongoose")
 
-/* GET home page. */
+var url='mongodb://localhost:27017/posts'
+
+const loginschema = mongoose.Schema({
+  username: String,
+  email: String,
+  pwd: String,
+});
+
+const member = mongoose.model('member', loginschema);
+
+
+
+
+
+
 router.post('/signup', (req, res) => {
   const data = req.body; // Extract data from request body
   console.log(data); // Log data to console
   res.send('Received data!!!!!'); // Send response back to clients
+
+  mongoose.connect(url, { useNewUrlParser: true })
+    .then(() => {
+     
+
+      if (data.pwd == data.pwdagain) {
+        const mem = new member({
+          username: data.username,
+          email: data.email,
+          pwd: data.pwd,
+        });
+
+        return mem.save(); // return the Promise
+      } else {
+        console.log("the passwords don't match");
+      }
+    })
+    .then(() => {
+      console.log('Member saved successfully');
+      mongoose.disconnect();
+    })
+    .catch(err => {
+      console.log(err);
+    });
 });
 module.exports = router;
